d8395d025fd8cfea705173f1c045b3f685e24147
sheharyarkhan1098 <sheharyar.khan1098@gmail.com>
2022-01-12 23:48:49 +0500

    sample layout top section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,33 +6,46 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    fontSize: 50
   },
   title: {
     flexGrow: 1,
+    fontWeight: 'bolder'
   },
+  navBar: {
+    backgroundColor: '#D4C1EC',
+    color: "black",
+  },
+  button: {
+    borderRadius: 20,
+    margin: 5
+  }
 }));
 
 function Navbar() {
   const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.navBar}>
+      <Container maxWidth="md"  style={{padding: 0}}>
   <Toolbar>
    
-    <Typography variant="h6" className={classes.title}>
-      News
+    <Typography variant="h4" className={classes.title}>
+      Cool Boys
     </Typography>
-    <Button color="inherit">Login</Button>
+    <Button color="inherit" variant="outlined" className={classes.button} >Join Discord</Button>
+    <Button color="inherit" variant="outlined" className={classes.button} >Open Wallet</Button>
     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
+      <MenuIcon className={classes.menuButton} />
     </IconButton>
   </Toolbar>
+  </Container>
 </AppBar>
   );
 }
--- src/components/topSection.js
+++ src/components/topSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+
+
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,23 +20,54 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  img: {
+    maxHeight: "90%",
+  },
+  h3: {
+    marginBottom: 20,
+    fontWeight: 'Bolder'
+  },
+  body2: {
+    marginBottom: 10,
+  },
+  subtitle1:{
+    fontWeight: 'bold'
+  }
 }));
+
+const height = 500;
 
 function TopSection() {
   const classes = useStyles();
   return (
-    <AppBar position="static">
-  <Toolbar>
-   
-    <Typography variant="h6" className={classes.title}>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-  </Toolbar>
-</AppBar>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl"  style={{padding: 0, backgroundColor: '#D4C1EC' }}>
+        
+          <Grid container style={{height: height}}>
+            <Grid item md={6} style={{justifyContent: 'center', alignItems: 'center', display: 'flex', height: height}}>
+              <Typography component="div" style={{width: 500, padding: 50 }}>
+                <Typography variant="h3" className={classes.h3}>
+                Cool Boys Exclusive Club
+                </Typography>
+                <Typography variant="body2" className={classes.body2}>
+                  A collection of 10,000 randomly generated metaverse friendly Cool Boys that grant you access to exclusive events such as yacht and mansion parties in the digital and physical world. Check out our discord for daily updates.
+                </Typography>
+                  <Typography variant="subtitle1" className={classes.subtitle1}>
+                  Pre Sale Mint: 5th February 2pm EST @ 0.06 ETH 
+                  </Typography>
+                  <Typography variant="subtitle1" className={classes.subtitle1}>
+                  Public Mint: 6th February 2pm EST @ 0.10 ETH 
+                  </Typography>
+              </Typography>
+            </Grid>
+            <Grid item md={6} style={{justifyContent: 'center', display: 'flex', height: height}} >
+            <img className={classes.img} src="/charaters.png" alt={"charaters"} />
+              </Grid>
+          </Grid>
+          
+      </Container>
+    </React.Fragment>
   );
 }
 
